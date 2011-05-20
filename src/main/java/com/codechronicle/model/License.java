e4909bd2818f8bbd973dbb086f8aa08285d26817
Saptarshi Roy <saptarshi.roy@salesforce.com>
2011-05-20 14:28:37 -0700

    Added most of the model, working on queries

--- src/main/java/com/codechronicle/model/License.java
+++ src/main/java/com/codechronicle/model/License.java
@@ -1,6 +1,24 @@
 package com.codechronicle.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * This entity describes a specific instance of a license. For example, "Apache 2". For simplicity, we do
+ * not incorporate pseudonyms (since there isn't really a standard for how different projects refer to the
+ * same license. This may be a future enhancement.
+ * 
+ * @author SRoy
+ *
+ */
+@Entity
 public class License {
+	
+	@Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+	private String id;
 	
 	private String name;
 	private String url;
@@ -11,6 +29,10 @@ public class License {
 		this.url = url;
 	}
 	
+	public String getId() {
+		return id;
+	}
+
 	public String getName() {
 		return name;
 	}
