ea5b1dd7059a1883b8b9a936bc29942156ccdd4f
Saptarshi Roy <saptarshi.roy@salesforce.com>
2011-06-02 17:50:58 -0700

    License controller starting to come alive with interactions

--- src/main/webapp/resources/admin/app/controller/LicensesController.js
+++ src/main/webapp/resources/admin/app/controller/LicensesController.js
@@ -1,46 +1,84 @@
 Ext.define('AM.controller.LicensesController', {
-    extend: 'Ext.app.Controller',
+	extend : 'Ext.app.Controller',
 
-    stores: ['LicensesStore'],
-    
-    models: ['License'],
-    
-    views: ['licenses.LicenseList',
-            'licenses.LicenseManagerView'],
-         
-    init: function() {
-    	
-    }
-    
-    /*
-     *  This is the previous working code
-     * 
-     * 
-    stores: ['LicensePolicies'],
-    
-    models: ['LicensePolicy'],
-    
-    views: ['AppMain'],
-         
-    init: function() {
-    	this.control({
-            'licensePolicyList': {
-                select: this.addLicense
-            }
-        });
-    },
-    
-    addLicense : function (field, value) {
-    	console.log('Double clicked on ' + field.getValue());
-    	
-    	var l = new AM.model.LicensePolicy({name : 'specialLicense'});
-    	console.log("Created new license policy : " + l);
-    	var licensePoliciesStore = this.getLicensePoliciesStore();
-    	console.log("LP store : " + licensePoliciesStore);
-    	
-    	licensePoliciesStore.add(l);
-    	licensePoliciesStore.sync();
-    }
-     */
-    
+	stores : [ 'LicensesStore' ],
+
+	models : [ 'License' ],
+
+	views : [ 'licenses.LicenseList', 'licenses.LicenseManagerView' ],
+
+	refs : [ {
+		ref : 'editButton',
+		selector : 'licensesList button[action=edit]'
+	}, {
+		ref : 'licenseGrid',
+		selector : 'licensesList'
+	} ],
+
+	init : function() {
+		this.control({
+			'licensesList' : {
+				itemclick : this.licenseSelected,
+				itemdblclick : this.doubleClickEdit
+			},
+			'licensesList button[action=edit]' : {
+				click : this.editSelected
+			},
+			'licensesList button[action=new]' : {
+				click : this.createNewLicense
+			}
+		});
+	},
+
+	licenseSelected : function() {
+		console.log("An item was clicked on the license grid");
+		var button = this.getEditButton();
+		button.setDisabled(false);
+	},
+	
+	doubleClickEdit:function(model, record) {
+		var licenseId = record.get('id');
+		this.editLicense(licenseId);
+	},
+
+	editSelected : function() {
+		var grid = this.getLicenseGrid();
+		var selectedItem = grid.getSelectionModel().getSelection()[0];
+		
+		var licenseId = selectedItem.get('id');
+		this.editLicense(licenseId);
+	},
+	
+	editLicense: function (licenseId) {
+		console.log("Starting edit of license id : " + licenseId);
+	},
+	
+	createNewLicense: function() {
+		console.log("Creating new license");
+	}
+
+/*
+ * This is the previous working code
+ * 
+ * 
+ * stores: ['LicensePolicies'],
+ * 
+ * models: ['LicensePolicy'],
+ * 
+ * views: ['AppMain'],
+ * 
+ * init: function() { this.control({ 'licensePolicyList': { select:
+ * this.addLicense } }); },
+ * 
+ * addLicense : function (field, value) { console.log('Double clicked on ' +
+ * field.getValue());
+ * 
+ * var l = new AM.model.LicensePolicy({name : 'specialLicense'});
+ * console.log("Created new license policy : " + l); var licensePoliciesStore =
+ * this.getLicensePoliciesStore(); console.log("LP store : " +
+ * licensePoliciesStore);
+ * 
+ * licensePoliciesStore.add(l); licensePoliciesStore.sync(); }
+ */
+
 });
--- src/main/webapp/resources/admin/app/view/licenses/LicenseList.js
+++ src/main/webapp/resources/admin/app/view/licenses/LicenseList.js
@@ -2,6 +2,17 @@ Ext.define('AM.view.licenses.LicenseList' ,{
     extend: 'Ext.grid.Panel',
     alias : 'widget.licensesList',
 
+    fbar: [
+           { type: 'button', 
+        	 text: 'New License',
+        	 action: 'new'},
+        	 
+           { type: 'button', 
+	       	 text: 'Edit Selected',
+	       	 disabled : true,
+             action: 'edit'}	 
+         ],
+    
     initComponent: function() {
     	
     	this.store = 'LicensesStore';
